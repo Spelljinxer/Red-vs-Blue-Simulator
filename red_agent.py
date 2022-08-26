3481e0e1c0be169f1ed1a1b0603830702d9ea0d4
Spelljinxer <reiden556@gmail.com>
2022-08-26 21:04:02 +0800

    add red agent

---
+++ red_agent.py
@@ -0,0 +1,9 @@
+class red_agent:
+    def __init__(self, message, potency, followerloss):
+        self.message = message
+        self.potency = potency
+        self.followerloss = followerloss
+
+
+a1 = red_agent("hello, join the read team", 2, 0)
+
