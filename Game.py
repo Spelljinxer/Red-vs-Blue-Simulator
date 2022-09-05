d75c4722c3c6839b6bc91ef01bc98c4296539e05
Spelljinxer <reiden556@gmail.com>
2022-09-05 17:26:58 +0800

    its compiling so all good OMEGALUL

--- Game.py
+++ Game.py
@@ -5,10 +5,22 @@ import green_agent
 import grey_agent
 
 class Game:
-    
+
     election_date = 0
-    
+    agent_total = 100 #total number of agents (modify later to match with tests)
+    red_agent = red_agent.red_agent()
+    blue_agent = blue_agent.blue_agent()
     def __init__(self, days):
         self.election_date = days
-        red_agent = red_agent()
-        blue_agent = blue_agent()
+        #make a list of green agents
+        
+        green_team = []
+        for i in range(0, self.agent_total - (10)):
+            green_team.append(green_agent.green_agent())
+
+
+g = Game(100)
+
+
+
+   
