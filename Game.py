15448b232bc7729241f0e36226e56d7b5513f392
Spelljinxer <reiden556@gmail.com>
2022-09-05 18:44:44 +0800

    more work

--- Game.py
+++ Game.py
@@ -7,9 +7,15 @@ import grey_agent
 class Game:
 
     election_date = 0
+    current_date = 0
+    
     agent_total = 100 #total number of agents (modify later to match with tests)
     red_agent = red_agent.red_agent()
     blue_agent = blue_agent.blue_agent()
+    
+    '''
+    Constructor for the Game
+    '''
     def __init__(self, days):
         self.election_date = days
         #make a list of green agents
@@ -17,9 +23,28 @@ class Game:
         green_team = []
         for i in range(0, self.agent_total - (10)):
             green_team.append(green_agent.green_agent())
-
+        
+        grey_team = []
+        for i in range(0, 10):
+            grey_team.append(grey_agent.grey_agent())
+    
+    '''
+    Increment to go the next round
+    '''
+    def next_day(self):
+        self.current_date += 1
+        if self.current_date == self.election_date:
+            #election day
+            print("It is Election Day!\n")
+            pass
+        else:
+            
+            #not election day
+            pass
 
 g = Game(100)
+print(g.election_date)
+
 
 
 
--- red_agent.py
+++ red_agent.py
@@ -1,10 +1,16 @@
-
+import random
 
 class red_agent:
 
 
     def __init__(self):
         pass
+
+
+    def uncertainity(self, potency):
+        uncertainity = potency - 1 + (random.random() * (potency - (potency - 1)))
+        return uncertainity
+    
 
 
 
