e0ba2eb248ad09a3e80f74b43570d0de03c50de5
Spelljinxer <reiden556@gmail.com>
2022-09-05 17:18:43 +0800

    more shit

--- Game.py
+++ Game.py
@@ -1,9 +1,14 @@
-
-
-from calendar import day_abbr
-
+#should this be a class or not?
+import red_agent
+import blue_agent
+import green_agent
+import grey_agent
 
 class Game:
+    
     election_date = 0
+    
     def __init__(self, days):
         self.election_date = days
+        red_agent = red_agent()
+        blue_agent = blue_agent()
