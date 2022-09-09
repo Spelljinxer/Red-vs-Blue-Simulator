c1b649ce1b3c515c5aa498568009ee2480094304
nathan eden <nathaneden483@yahoo.com.au>
2022-09-09 21:04:57 +0800

    added extremely detailed commenting

--- grey_agent.py
+++ grey_agent.py
@@ -3,6 +3,8 @@
 class grey_agent:
     
     def __init__(self, team, id):
+        #which team they are working for
         self.team = team
+        #agents unique ID
         self.id = id
         
