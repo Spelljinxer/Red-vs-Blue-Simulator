d4c0a6909b0973ccb120c9ec9f447bec307fa3aa
Spelljinxer <reiden556@gmail.com>
2022-09-10 17:55:23 +0800

    idk blue agent params

--- blue_agent.py
+++ blue_agent.py
@@ -1,5 +1,9 @@
 
 class blue_agent:
     
-    def __init__(self):
-        pass
+    def __init__(self, energy_level):
+        self.energy_level = energy_level
+
+    def blue_move(self, green_team):
+        for agent in green_team:
+            pass
--- green_agent.py
+++ green_agent.py
@@ -2,7 +2,8 @@
 
 class green_agent:
     
-    def __init__(self, vote_status, uncertainty, opinion):
+    def __init__(self, unique_id, vote_status, uncertainty, opinion):
+        self.unique_id = unique_id
         self.vote_status = vote_status
         self.uncertainty = uncertainty
         self.opinion = opinion
