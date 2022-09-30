2a44722d6b537e7d7e4fd025afc3e3bddb748e25
nathan eden <nathaneden483@yahoo.com.au>
2022-09-30 19:29:24 +0800

    change of vote status in blue agent

--- blue_agent.py
+++ blue_agent.py
@@ -24,6 +24,44 @@ class blue_agent:
         self.energy_level = energy_level
         self.followers = 0
         self.user_playing = user_playing
+    
+    def get_message_certainty_energy_loss(self, message):
+        certainty = 0.0
+        energy_loss = 0.0
+        if message == self.messages[10]:
+            #idk yet
+            return True
+        elif message == self.messages[0]:
+            certainty = 0.1
+            energy_loss = 0.005
+        elif message == self.messages[1]:
+            certainty = 0.2
+            energy_loss = 0.01
+        elif message == self.messages[2]:
+            certainty = 0.3
+            energy_loss = 0.015
+        elif message == self.messages[3]:
+            certainty = 0.4
+            energy_loss = 0.02
+        elif message == self.messages[4]:
+            certainty = 0.5
+            energy_loss = 0.025
+        elif message == self.messages[5]:
+            certainty = 0.6
+            energy_loss = 0.03
+        elif message == self.messages[6]:
+            certainty = 0.7
+            energy_loss = 0.035
+        elif message == self.messages[7]:
+            certainty = 0.8
+            energy_loss = 0.04
+        elif message == self.messages[8]:
+            certainty = 0.9
+            energy_loss = 0.045
+        elif message == self.messages[9]:
+            certainty = 1.0
+            energy_loss = 0.05
+        return [certainty, energy_loss]
 
     def valid_move(self, output, choice, green_team, grey_team):
         if(int(choice) > len(output) or int(choice) < 1):
@@ -31,6 +69,11 @@ class blue_agent:
             self.blue_move(green_team, grey_team)
         else:
             print("You have chosen: " + output[int(choice) - 1])
+    
+    def will_vote_status_change(self, certainty):
+        chance = certainty * 100
+        if (random.randint(0, 100) <= chance):
+            return True
     
     def blue_move(self, green_team, grey_team):
         #if the human is playing as the blue agent
@@ -51,7 +94,19 @@ class blue_agent:
         
         #Execute the rest of the code if human is not playing as blue agent
         for green_agent in green_team:
-            pass
+            uncertainty = 0
+            #placeholder until we map the user input/AI choice to this variable 
+            message = "RUN YOUR BOY OVER THE GRASS"
+            #handle uncertainty change here
+            
+            #handle chance of opinion change and change (or no change) here
+            certainty_energy_loss = self.get_message_certainty_energy_loss(message)
+            certainty = certainty_energy_loss[0]
+            energy_loss = certainty_energy_loss[1]
+            will_it = self.will_vote_status_change(certainty)
+            if (will_it == True):
+                green_agent.vote_status == True
+            
 
         #passive buff
         self.energy_level += (0.01 * self.followers)
